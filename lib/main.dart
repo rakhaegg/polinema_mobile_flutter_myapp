a8223f7c7d5f687cbdb8d7b7a62301c0f2a689e2
rakhaegg <rakhae.g@gmail.com>
2021-09-30 18:15:00 +0700

    push

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,25 +11,239 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    MainAxisAlignment mainAxisAlignment;
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("MyApp"),
+        ),
+
+      
+        body: ListView(
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: <Widget> [
+                  Container(
+                    decoration: BoxDecoration(
+                      color : Colors.amberAccent
+                    ),
+                    child: Text("BERITA TERBARU",
+                    textAlign: TextAlign.center),
+                    width: 211,
+                    height: 60,
+                    padding: EdgeInsets.all(20),
+                    
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                      color : Colors.amberAccent
+                    ),
+                    child: Text("PERTANDINGAN HARI INI",
+                    textAlign: TextAlign.left,),
+                        width: 200,
+                    height: 60,
+                    padding: EdgeInsets.all(20),
+
+                  )
+                ],
+              ),
+              Column(
+                children: <Widget>[
+                  Container(
+                    child :
+                    Image.network("https://cdn0-production-images-kly.akamaized.net/DD2wBTHWq3qv7_jo5rZBrSfSbt4=/640x360/smart/filters:quality(75):strip_icc():format(jpeg)/kly-media-production/medias/1819629/original/084908600_1515011807-Diego_Costa.jpg"),
+                  )
+                ],
+              ),
+              Column(
+                children:<Widget> [
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.amberAccent
+                    ),
+                    child: Text("Transfer",
+                    textAlign: TextAlign.left,),
+                    width: 500,
+                    height: 50,
+                    padding: EdgeInsets.only(top: 15,left: 5),
+                  )
+                ],
+              ),
+              Column(
+                children: <Widget>[
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Container(
+                        child: Image.network("https://cdn1-production-images-kly.akamaized.net/oi0AdoHWcJNa6-8_H4BJMyPZLr8=/640x360/smart/filters:quality(75):strip_icc():format(jpeg)/kly-media-production/medias/2739791/original/055626800_1551318498-20190227-Real-Madrid-Vs-Barcelona-AP-7.jpg"),
+                        width: 200,
+                        height: 100,
+                        decoration: BoxDecoration(
+                          border: Border.all(color : Colors.blueAccent),
+                        ),
+                      ),
+                      Container(
+                        child: Text('Pique Bilang Wasit Untungkan'
+                        'Madrid Koeman Tepok Jidat'),
+                        width: 200,
+                        height: 100,
+                        decoration: BoxDecoration(
+                          border : Border.all(color : Colors.blueAccent),
+                        ),
+                      ),
+                      
+                    ],
+                  ),
+                  Column(
+                    children: <Widget>[
+                      Container(
+                        width: 500,
+                        height: 50,
+                        child: Text("Barcelona Feb 13 2021"),
+                        decoration: BoxDecoration(
+                          border: Border.all( color : Colors.blueAccent),
+                        
+                        ),
+                        padding: EdgeInsets.only(top: 10 , left: 5),
+
+                      )
+                    ],
+                  )
+                ],
+              ),
+              Column(
+                children: <Widget>[
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Container(
+                        child: Image.network("https://cdn1-production-images-kly.akamaized.net/oi0AdoHWcJNa6-8_H4BJMyPZLr8=/640x360/smart/filters:quality(75):strip_icc():format(jpeg)/kly-media-production/medias/2739791/original/055626800_1551318498-20190227-Real-Madrid-Vs-Barcelona-AP-7.jpg"),
+                        width: 200,
+                        height: 100,
+                        decoration: BoxDecoration(
+                          border: Border.all(color : Colors.blueAccent),
+                        ),
+                      ),
+                      Container(
+                        child: Text('Pique Bilang Wasit Untungkan'
+                        'Madrid Koeman Tepok Jidat'),
+                        width: 200,
+                        height: 100,
+                        decoration: BoxDecoration(
+                          border : Border.all(color : Colors.blueAccent),
+                        ),
+                      ),
+                      
+                    ],
+                  ),
+                  Column(
+                    children: <Widget>[
+                      Container(
+                        width: 500,
+                        height: 50,
+                        child: Text("Barcelona Feb 13 2021"),
+                        decoration: BoxDecoration(
+                          border: Border.all( color : Colors.blueAccent),
+                        
+                        ),
+                        padding: EdgeInsets.only(top: 10 , left: 5),
+
+                      )
+                    ],
+                  )
+                ],
+              ),
+              Column(
+                children: <Widget>[
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Container(
+                        child: Image.network("https://cdn1-production-images-kly.akamaized.net/oi0AdoHWcJNa6-8_H4BJMyPZLr8=/640x360/smart/filters:quality(75):strip_icc():format(jpeg)/kly-media-production/medias/2739791/original/055626800_1551318498-20190227-Real-Madrid-Vs-Barcelona-AP-7.jpg"),
+                        width: 200,
+                        height: 100,
+                        decoration: BoxDecoration(
+                          border: Border.all(color : Colors.blueAccent),
+                        ),
+                      ),
+                      Container(
+                        child: Text('Pique Bilang Wasit Untungkan'
+                        'Madrid Koeman Tepok Jidat'),
+                        width: 200,
+                        height: 100,
+                        decoration: BoxDecoration(
+                          border : Border.all(color : Colors.blueAccent),
+                        ),
+                      ),
+                      
+                    ],
+                  ),
+                  Column(
+                    children: <Widget>[
+                      Container(
+                        width: 500,
+                        height: 50,
+                        child: Text("Barcelona Feb 13 2021"),
+                        decoration: BoxDecoration(
+                          border: Border.all( color : Colors.blueAccent),
+                        
+                        ),
+                        padding: EdgeInsets.only(top: 10 , left: 5),
+
+                      )
+                    ],
+                  )
+                ],
+              ),
+              
+            ],          
+          ),
+
+        )
+      );
   }
 }
+class ListViewh extends StatelessWidget{
+  
+  @override
+  Widget build(BuildContext context){
+    return ListView(
+       padding :const EdgeInsets.all(8),
+       children: const <Widget>[
+         ListTile(
+           title: Text('List1'),
+         ),
+         ListTile(
+           title: Text('List1'),
+         ),
+         ListTile(
+           title: Text('List1'),
+         ),
+       ],
+    );
+  } 
+}
+Widget buatKotak(String tex , Color warna , double ukuran){
+  return Container(
+    
+    decoration:   BoxDecoration(
+       color : warna,
+       
+    ),
+    child : Text(
+      tex,
+      textAlign: TextAlign.center,
+     
+      ),
+    height: ukuran,
+    width : ukuran,
+    margin: EdgeInsets.only(left:100.0,right: 20.0),
+    padding: EdgeInsets.fromLTRB(20, 20, 20, 20),
+    
 
+  );
+}
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
 
